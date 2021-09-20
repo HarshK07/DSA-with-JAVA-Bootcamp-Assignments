f4de2ce64c3095ca471b5606c7b6e7e82ff0b349
Mansi Mishra <mansimishra510@gmail.com>
2021-09-20 12:37:21 -0500

    Add files via upload

---
+++ Array/array.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+class array{
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] =1;
+        arr[1] =2;
+        arr[2] =3;
+        System.out.println(Arrays.toString(arr));
+    
+
+    String name[] = {"mansi","nan","gvb"};
+    System.out.println(Arrays.toString(name));
+  }
+}
